af32d6edaaa1c8fd0c69bdb4169aea0e7e94c146
DMakKin <Onecard1c@gmail.com>
2024-11-21 19:21:07 +0200

    1st
    Added new Controller, and clear project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed; // Скорость движения
+    [SerializeField] private float jumpForce; // Сила прыжка
+    [SerializeField] private LayerMask groundLayer; // Слой земли
+    [SerializeField] private Transform groundCheck; // Точка проверки земли
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    
+    private float moveInputX;
+    private float moveInputZ;
+    private Rigidbody rb;
+    //private Animator animator;
+    private bool isGrounded;
+    private Vector3 movement;
+    private Vector3 newVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+        //Animate();
+    }
+
+    void Move()
+    {
+        moveInputX = Input.GetAxis("Horizontal");
+        moveInputZ = Input.GetAxis("Vertical");
+        movement = new Vector3(moveInputX, 0f, moveInputZ) * moveSpeed;
+        newVelocity = new(movement.x, rb.velocity.y, movement.z);
+        rb.velocity = newVelocity;
+
+        if (moveInputX != 0 || moveInputZ != 0)
+        {
+            transform.forward = new Vector3(moveInputX, 0, moveInputZ); // Поворачиваем персонажа в сторону движения
+        }
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    //void Animate()
+    //{
+    //    animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x)); // Скорость для анимации бега
+    //    animator.SetBool("IsGrounded", isGrounded); // Условие для анимации прыжка
+    //}
+}
